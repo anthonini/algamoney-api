c0f0eccd7af9b4a0a12e77cd395fb889da30a2fd
Anthonini <anthonini@gmail.com>
2021-07-11 13:30:30 -0300

    Incluido o campo cep em endereco.
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/java/com/example/algamoney/api/model/Endereco.java
+++ src/main/java/com/example/algamoney/api/model/Endereco.java
@@ -8,7 +8,8 @@ public class Endereco {
 	private String logradouro;	
 	private String numero;	
 	private String complemento;	
-	private String bairro;	
+	private String bairro;
+	private String cep;
 	private String cidade;	
 	private String estado;
 
@@ -42,6 +43,14 @@ public class Endereco {
 
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 	public String getCidade() {
